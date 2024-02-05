1b6e9f4533a072b7a407607d94160e035ba2aa26
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2024-02-05 16:38:38 +0100

    Add decoding functionality to decoder.py

--- decoder.py
+++ decoder.py
@@ -4,3 +4,13 @@ def decode(image):
     decoded_objects = pyzbar.decode(image)
     return decoded_objects
 
+if __name__ == "__main__":
+    
+    
+    """
+    N = int(input("Enter the number of times to decode: "))
+    image = "flag.png"
+    for i in range(N):
+        image = decode(image)
+        print(image)
+    """
